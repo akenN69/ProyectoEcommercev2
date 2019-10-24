0384468e81603c3fc5d00cc50225db83990825d4
Seba_virtual <rata_6952@hotmail.com>
2019-10-24 19:42:47 -0300

    add gem paypal

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show, :new] do
     resources :orders, only: :create
   end
-  resources :orders, only: [:index]
+  
   resources :products
   devise_for :users
   resources :users
+  resources :orders, only: :index do
+  	collection do
+  		get 'clean'
+  	end
+  end
   resources :billings, only: [] do
     collection do
       get 'pre_pay'
+      get 'execute'
     end
-  end
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end
